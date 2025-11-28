8dfbda8c95b84b7a1c46c7920845ab3d333c22ed
Alexandra Helmersson <alexandra.helmersso@hotmail.com>
2025-11-28 15:00:53 +0100

    AppMvc med ny Overview vy
    Grunden för projektet i Mvc-delen

--- AppMvc/Controllers/HomeController.cs
+++ AppMvc/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AppMvc.Models;
+using Services.Interfaces;
 
 namespace AppMvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    //private readonly IAddressesService _addressesService;
+    private readonly IAdminService _adminService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IAddressesService addressesService, IAdminService adminService)
     {
         _logger = logger;
+        //_addressesService = addressesService;
+        _adminService = adminService;
     }
 
     public IActionResult Index()
@@ -23,6 +28,14 @@ public class HomeController : Controller
         return View();
     }
 
+    public async Task<IActionResult> Overview()
+        {
+            var viewModel = new OverviewViewModel();
+            //var addresses = await _addressesService.ReadAddressesAsync(true, false, "Denmark", 0 ,50);
+            var info = await _adminService.GuestInfoAsync();
+            viewModel.CountryInfo = info.Item.Friends.Where(f=> f.Country == "Denmark");
+            return View(viewModel);
+        }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
